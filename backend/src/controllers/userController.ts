5bc938ca4f5f29918b2815ebeaa4d560c330e49a
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2025-12-10 03:14:08 +0530

    some changes

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -82,7 +82,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("token", token, {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000
 });
 
